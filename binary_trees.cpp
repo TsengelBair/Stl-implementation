84e3c377eccde76b71f4e0bbb031ef3cb89df5e6
Bair <tsengel01@bk.ru>
2024-12-18 16:40:39 +0300

    added remove function with balancing tree after remove

--- binary_trees.cpp
+++ binary_trees.cpp
@@ -38,6 +38,11 @@ public:
         }
     }
 
+    /* при операции удаления обновляется все дерево */
+    void remove(T key) {
+        _root = removeNode(_root, key);
+    }
+
 private:
     void insertInRightPlace(Node<T>* newNode, Node<T>* parent) {
         /* если меньше либо равен родителю, вставляем влево */
@@ -69,12 +74,65 @@ private:
         if (key == parent->_key) {
             return parent;
         }
-        else if (key <= parent->_key) {
+        else if (key < parent->_key) {
             return findNode(key, parent->_left);
         }
         else {
             return findNode(key, parent->_right);
         }
+    }
+
+    Node<T>* removeNode(Node<T>* node, T key) {
+        if (node == nullptr) return nullptr;
+        /*
+            ищем нужную вершину рекурсивно по ключу
+            базовый случай наступит в else
+        */
+        if (key < node->_key) {
+            node->_left = removeNode(node->_left, key);
+        }
+        else if (key > node->_key) {
+            node->_right = removeNode(node->_right, key);
+        }
+        /* нужную вершину получили */
+        else {
+            /* если это лист */
+            if (node->_left == nullptr && node->_right == nullptr) {
+                delete node;
+                return nullptr;
+            }
+            /* если узел слева отсутствует, заменяем удаляемый узел на его правого ребенка */
+            else if (node->_left == nullptr && node->_right != nullptr) {
+                Node<T>* tmp = node->_right;
+                delete node;
+                return tmp;
+            }
+            /* заменяем на левого ребенка */
+            else if (node->_left != nullptr && node->_right == nullptr) {
+                Node<T>* tmp = node->_left;
+                delete node;
+                return tmp;
+            }
+            /* нужная вершина найдена */
+            else {
+                /*
+                    если у удаляемого узла есть два ребенка, нужно найти максимальный из его левого поддерева
+                    либо максимальный из левого поддерева
+                */
+                Node<T>* maxFromLeft = findMaxFromLeft(node);
+                node->_key = maxFromLeft->_key; /* копируем значение */
+                node->_left = removeNode(node->_left, maxFromLeft->_key); /* удаляем максимальный узел */
+            }
+        }
+        return node;
+    }
+
+    Node<T>* findMaxFromLeft(Node<T>* node) {
+        while (node->_left != nullptr) {
+            node = node->_left;
+        }
+        /* максимальный эл-т из левого поддерева */
+        return node = node->_right;
     }
 
     void Destroy(Node<T>* node) {
@@ -88,18 +146,3 @@ private:
 private:
     Node<T>* _root;
 };
-
-int main()
-{
-    Tree<int>binary_tree;
-
-    binary_tree.insert(10);
-    binary_tree.insert(5);
-    binary_tree.insert(12);
-
-    Node<int>*node = binary_tree.find(5);
-    std::cout << node->_key;
-
-    return 0;
-}
-
